e0ecc5b0100ed6ef2e7140c363aa45c5ce68cf9a
oswiux <35800600+oswiux@users.noreply.github.com>
2021-01-26 14:34:48 +0200

    gut

--- intro/testinis.js
+++ intro/testinis.js
@@ -1,10 +1,12 @@
 
 console.log("...............");
 function sudetis(x){
+    console.log(x);
     sum = 0 ;
     for (let i = 0; i < x.length; i++)
     sum += x[i];
     return sum;
+    
 }
 
 console.log(sudetis([1, 3, 3, 4, 5]), 15);
